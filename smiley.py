3fc075230f03f1dfb9f9ef7cb6e388036276dac1
KannanRishitha-codes <kannanrishitha@gmail.com>
2026-09-06 16:48:29 +0800

    Update smiley.py

--- smiley.py
+++ smiley.py
@@ -2,14 +2,14 @@ def smiley():
     import time
     for i in range(1,19):
         if i%2==0:
-            print("  $ $  ")
-            print("   o   ")
+            print("  ^ ^  ")
+            print("  ---  ")
             print()
             print()
             time.sleep(1)
         else:
-            print("  * *  ")
-            print("   #  ")
+            print("  0 0 ")
+            print("   o  ")
             print()
             print()
             time.sleep(1)
